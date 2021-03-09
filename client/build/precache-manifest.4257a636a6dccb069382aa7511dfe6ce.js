88b13ec5daa51d7a63a0a401ab640e4acfde4732
Shannon Quinn <shannon.quinn91@gmail.com>
2021-03-08 20:23:59 -0500

    Deploy 2

---
+++ client/build/precache-manifest.4257a636a6dccb069382aa7511dfe6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c258cfc98b01f4954cc45420e523bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f996eff8f31e91d1ff87",
+    "url": "/static/css/2.af04be4e.chunk.css"
+  },
+  {
+    "revision": "70698e86e6861899c624",
+    "url": "/static/css/main.337ca1a3.chunk.css"
+  },
+  {
+    "revision": "f996eff8f31e91d1ff87",
+    "url": "/static/js/2.cf34f3a7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cf34f3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70698e86e6861899c624",
+    "url": "/static/js/main.5360f2de.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
